2d05bf49be8f408e3d686048e436881734b3c10a
Michael Karrer <michaelkarrer81@gmail.com>
2017-08-03 00:33:31 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/hema_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'hema_config',
+    'summary': '''FS-Online hema instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance hema
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
